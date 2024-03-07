9fd9be4bc12cc2316c866528ecf3ab06b926b187
Srilatha Maddineni <srilatha.maddineni17@gmail.com>
2024-03-07 16:54:03 -0500

    Adding changes in the ___init.py file

--- Gentopia/gentopia/tools/__init__.py
+++ Gentopia/gentopia/tools/__init__.py
@@ -13,6 +13,7 @@ from .gradio import *
 from .code_interpreter import PythonCodeInterpreter
 from .file_operation import WriteFile, ReadFile
 from .duckduckgo import DuckDuckGo
+from .pdf_reader import ReadPdf
 
 
 def load_tools(name: str) -> BaseTool:
@@ -43,6 +44,7 @@ def load_tools(name: str) -> BaseTool:
         "search_single_paper": SearchSinglePaper,
         "search_related_paper": SearchRelatedPaper,
         "search_cite_paper": SearchCitePaper,
+        "read_pdf": ReadPdf
     }
     if name not in name2tool:
         raise NotImplementedError
